123089ea2ead7eb3fee8651c23560a0005948452
mthanki <mehulthanki13@gmail.com>
2023-08-15 21:30:49 -0400

    how to do it

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>And there is nothing here, not yet</p>
+        <p>
+          why is it that simply tryiong to kill yourself is so fucking difficult
+          I don't even want anything all I wanted to know are whay pills
+          actually kill you easily fuck
+        </p>
       </header>
     </div>
   );

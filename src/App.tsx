a9da52090064089807f7a33ea98019eade06888b
mthanki <mehulthanki13@gmail.com>
2023-08-17 00:49:27 -0400

    chanc

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>And there is nothing here, not yet</p>
-        <p>
-          why is it that simply tryiong to kill yourself is so fucking difficult
-          I don't even want anything all I wanted to know are whay pills
-          actually kill you easily fuck
-        </p>
+        <p>you know what, I changed my mind</p>
       </header>
     </div>
   );
